6326387cbd4f2e731cd02bf60529cb06b7ed4960
Vignesh Sankar Iyer <vignesh.sankar.iyer.1999@gmail.com>
2024-03-04 18:42:41 +0800

    add audiosegmenter

---
+++ audiosegmenter.go
@@ -0,0 +1,96 @@
+package audiosegmenter
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"strconv"
+	"sync"
+
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
+)
+
+type FormatInfo struct {
+	Duration string `json:"duration"`
+}
+
+type FFProbeOutput struct {
+	Format FormatInfo `json:"format"`
+}
+
+func ParseDuration(probeOutput string) (float64, error) {
+	var probe FFProbeOutput
+	err := json.Unmarshal([]byte(probeOutput), &probe)
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to unmarshal probe output: %w", err)
+	}
+
+	duration, err := strconv.ParseFloat(probe.Format.Duration, 64)
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse duration: %w", err)
+	}
+
+	return duration, nil
+}
+
+func CopyAudioSegment(inputFilePath string, segmentIdx int, segmentStart int, segmentDuration int, outputDir string) error {
+	outputFilePath := fmt.Sprintf("%s_seg_%d.mp3", inputFilePath, segmentIdx)
+
+	err := ffmpeg_go.Input(inputFilePath).Output(outputFilePath, ffmpeg_go.KwArgs{
+		"ss": strconv.Itoa(segmentStart),
+		"t":  strconv.Itoa(segmentDuration),
+		"c":  "copy",
+		"y":  "",
+	}).Run()
+
+	if err != nil {
+		return fmt.Errorf("failed to create segment %d for %s: %w", segmentIdx, outputFilePath, err)
+	}
+
+	return nil
+}
+
+func SegmentAudio(inputFilePath string, segmentDuration int, outputDir string) []error {
+	probeOutput, err := ffmpeg_go.Probe(inputFilePath)
+
+	if err != nil {
+		return []error{err}
+	}
+
+	duration, err := ParseDuration(probeOutput)
+
+	if err != nil {
+		return []error{err}
+	}
+
+	numberOfSegments := int(math.Ceil(duration / float64(segmentDuration)))
+
+	errChan := make(chan error, numberOfSegments)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < numberOfSegments; i++ {
+		wg.Add(1)
+
+		go func(segmentIdx int) {
+			defer wg.Done()
+			err := CopyAudioSegment(inputFilePath, segmentIdx, segmentIdx*segmentDuration, segmentDuration, outputDir)
+
+			if err != nil {
+				errChan <- fmt.Errorf("error processing segment %d: %w", segmentIdx, err)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	var errs []error
+	for e := range errChan {
+		errs = append(errs, e)
+	}
+
+	return errs
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module raga-recog-pipeline
 
 go 1.21.5
+
+require github.com/u2takey/ffmpeg-go v0.5.0
+
+require (
+	github.com/aws/aws-sdk-go v1.50.30 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
+)
